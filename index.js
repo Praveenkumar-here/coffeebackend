b021e3be71b3f4ab89cb24e87c49d8600152f764
PraveenKumar-here <66293833+Praveenkumar-here@users.noreply.github.com>
2024-05-06 20:41:42 +0530

    Update index.js
    Port update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 // import express from "express"
 const app = express()
-//const port = 4000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
