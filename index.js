f89357302612d7234f097cfaff77d1ccb51b27bb
Praveenkumar-here <praveenmitnick10110@gmail.com>
2024-05-07 23:17:17 +0530

    Done with bugs

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 // import express from "express"
 const app = express()
-<<<<<<< HEAD
 //const port = 4000
 const path = require('path')
 
@@ -10,9 +9,6 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 //app.use('/js', express.static(path.join(_dirname, 'node_modules/jquery/dist')))
 
-=======
-const port = 4000
->>>>>>> 82ab5614f53fefb35db3b155b71b2923eb552494
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -24,11 +20,7 @@ app.get('/login', (req,res) => {
 })
 
 app.get('/medium', (req,res) => {
-<<<<<<< HEAD
     res.send('<h1>Blog here!!!!!!!!!!!!!!</h1>')
-=======
-    res.send('<h1>Blog Will come up here!</h1>')
->>>>>>> 82ab5614f53fefb35db3b155b71b2923eb552494
 })
 
 app.get("/d", (req, res) => {
@@ -41,5 +33,5 @@ app.get('/Coffee', (req,res) => {
 
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
