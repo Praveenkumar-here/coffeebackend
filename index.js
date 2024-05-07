86b2f55e0a38935cedf95f47c8aa24af0e6e4f5b
Praveenkumar-here <praveenmitnick10110@gmail.com>
2024-05-07 23:06:13 +0530

    Just added some frontend

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 // import express from "express"
 const app = express()
 //const port = 4000
+const path = require('path')
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+//app.use('/js', express.static(path.join(_dirname, 'node_modules/jquery/dist')))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,8 +20,12 @@ app.get('/login', (req,res) => {
 })
 
 app.get('/medium', (req,res) => {
-    res.send('<h1>Blog here</h1>')
+    res.send('<h1>Blog here!!!!!!!!!!!!!!</h1>')
 })
+
+app.get("/d", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'))
+});
 
 app.get('/Coffee', (req,res) => {
     res.send('<h2> Lets Have some Coffee!')
